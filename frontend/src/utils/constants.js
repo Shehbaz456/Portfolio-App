a7c5466efbc598ca6943b9df1ad0b9f9bb64f685
shehbaz <mdshehbaz666666@gmail.com>
2025-10-29 13:39:02 +0530

    setup frontend project

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,28 @@
+export const TEMPLATES = [
+  {
+    id: 'templateA',
+    name: 'Template 1',
+    description: 'Modern and clean design with yellow hero section and professional layout',
+    image: '/template1-preview.jpg',
+    features: [
+      'Yellow Hero Section',
+      'Testimonials Carousel',
+      'Grid Portfolio',
+      'Contact Form'
+    ],
+    color: 'yellow'
+  },
+  {
+    id: 'templateB',
+    name: 'Template 2',
+    description: 'Split-screen layout with timeline skills and masonry portfolio grid',
+    image: '/template2-preview.jpg',
+    features: [
+      'Split Hero Layout',
+      'Timeline Skills',
+      'Masonry Portfolio',
+      'Blog Section'
+    ],
+    color: 'blue'
+  }
+];
